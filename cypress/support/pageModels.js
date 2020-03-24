a60c02e1ffc4e5133a6bfb35a45dd37a1e79bba0
meriem26 <mmlys89@gmail.com>
2020-03-24 15:42:33 +0000

    Add todos test suite to Travis CI

--- cypress/support/pageModels.js
+++ cypress/support/pageModels.js
@@ -1,5 +1,19 @@
 export const addNewTodo = todo => {
-    cy.get(".new-todo")
-      .type(todo)
-      .type("{enter}");
-  };
+  cy.get(".new-todo")
+    .type(todo)
+    .type("{enter}");
+};
+
+export const markItemAsCompleted = num => {
+  cy.get(`.todo-list li:nth-child(${num}) .toggle`).click();
+};
+
+export const getFilterByName = name => {
+  cy.get(".footer")
+    .contains(name)
+    .click();
+};
+
+export const assertNumberOfTodos = num => {
+  cy.get(".todo-list li").should("have.length", num);
+};
