6ab475c88b674b13785ff0d2b01f9a7fa4fdb22d
meriem26 <mmlys89@gmail.com>
2020-03-24 10:49:38 +0000

    Abstract entering todos into a function

--- cypress/integration/toDo.spec.js
+++ cypress/integration/toDo.spec.js
@@ -1,8 +1,28 @@
+const addNewTodo = todo => {
+  cy.get(".new-todo")
+    .type(todo)
+    .type("{enter}");
+};
 describe("To do page", function() {
   beforeEach(() => {
     cy.visit("/");
   });
-  it('should have title', () => {
-      cy.title().should('equal', 'React • TodoMVC')
-  })
+  it("Should have title", () => {
+    cy.title().should("equal", "React • TodoMVC");
+  });
+  describe("Should add new todos", () => {
+    beforeEach(() => {
+      addNewTodo("Go shopping");
+      addNewTodo("Call doctor");
+      addNewTodo("Have lunch");
+    });
+    it("Should contain a list of todos", () => {
+      cy.get(".todo-list").should("be.visible");
+    });
+    it("Delete an item", () => {});
+    it("Should mark an item as completed", () => {});
+    it("Display only active todos", () => {});
+    it("Display only completed todos", () => {});
+    it("Display the number of the left items", () => {});
+  });
 });
